7c1d8237307642d506d5205c8ce989a4c1a8ac27
S-G-mathematics <shivansh231223@gmail.com>
2026-07-10 14:29:58 +0530

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        int q=strs[0].size();
+        string x="";
+        for(int j=0;j<q;j++){
+            char v=strs[0][j];
+        for(int i=0;i<n;i++){
+            if(v!=strs[i][j]) return x;
+        }
+            x=x+v;
+        }
+        return x;
+    }
+};
